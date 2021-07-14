b5c213a4db07892183c0638ce7e231c696d2f719
RiddhiDamani <riddhidamani90@gmail.com>
2021-07-13 21:13:59 -0500

    web data with python

--- Ch5/inetdata_start.py
+++ Ch5/inetdata_start.py
@@ -2,5 +2,16 @@
 # Example file for retrieving data from the internet
 #
 
+# this module provides the classes and code that will be needed to make the http request. 
+import urllib.request
+
+
+def main():
+  webUrl = urllib.request.urlopen("http://www.google.com")
+  print("result code: " + str(webUrl.getcode()))
+  data = webUrl.read()
+  print(data)
+
+
 if __name__ == "__main__":
   main()
